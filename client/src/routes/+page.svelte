e6290a2c7bc11836cac263424bde5c6263d8a844
0ri0nexe <orionexe@outlook.fr>
2026-02-13 22:08:25 +0100

    Frontend now dynamic, clean json routes Vite proxy to avoid cors

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome on Dashy !</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+
+  let message = "Loading...";
+
+  onMount(async () => {
+    try {
+      const res = await fetch("/api/hello");
+      const data = await res.json();
+      message = data.message;
+    } catch (err) {
+      message = "Error connecting to backend";
+      console.error(err);
+    }
+  });
+</script>
+
+<h1>{message}</h1>
+
+<style>
+  h1 {
+    font-family: system-ui;
+    text-align: center;
+    margin-top: 40vh;
+  }
+</style>
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,12 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{Json, Router, routing::get};
 use tower_http::services::ServeDir;
 
+use serde::Serialize;
+use tower_http::cors::{CorsLayer, Any};
 
 #[tokio::main]
 async fn main() {
+
 	let app = static_frontend();
 	let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
 	let socket_addr = listener.local_addr().unwrap();
@@ -20,4 +23,16 @@ fn static_frontend() -> Router {
 
 	// Generate the router to use the root path
 	Router::new().fallback_service(static_frontend_dir)
+		.route("/api/hello", get(hello))
+}
+
+#[derive(Serialize)]
+struct HelloResponse {
+    message: String,
+}
+
+async fn hello() -> Json<HelloResponse> {
+    Json(HelloResponse {
+        message: "Hello from Rust backend 🦀".to_string(),
+    })
 }
